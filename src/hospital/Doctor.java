d4f747fb4c41f8a55d13f598e18c2280739557c8
Angelina <07140714no@gmail.com>
2019-03-24 21:02:02 +0300

    Больничка от больного

--- src/hospital/Doctor.java
+++ src/hospital/Doctor.java
@@ -1,17 +1,51 @@
 package hospital;
+
+import java.util.Scanner;
+
 public class Doctor
 {
-    String lor = "lor";
-    String lcode = "lor742";
+   String name;
+   String code;
+   String[] note = new String[10];
     
-    String terapevt = "terapevt";
-    String tcode = "terapevt742";
-    
-    String hirurg = "hirurg";
-    String hcode = "hirurg742";
-    
-    void work()
+   
+   public Doctor(String name, String code)
+   {
+       this.name = name;
+       this.code = code;
+   }
+    static void work(Doctor lor, Doctor hirurg, Doctor terapevt)
     {
-        System.out.println("Работа врача");
+        System.out.println("Ваши записи");
+        String answer;
+        do
+            {
+                System.out.println("Введите свой код");
+                System.out.println("Для выхода введите exit");
+                Scanner in = new Scanner(System.in);
+                answer = in.nextLine();
+                switch(answer)
+                {
+                    case "sasha742": lor.out(); break;
+                    case "artem742": hirurg.out(); break;
+                    case "ilya742": terapevt.out(); break;
+                    default:
+                        System.out.println("Вы ввели неправильно");
+                }
+            }while(!"exit".equals(answer));
+    }
+    
+    public void out()
+    {
+        int i = 0;
+        if(note[i] == null)
+        {
+            System.out.println("Нет записей");
+        }
+            while(note[i] != null)
+        {
+            System.out.println(note[i]);
+            ++i;
+        }
     }
 }
